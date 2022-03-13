a78eda05d9719a48fd62e5e4c5eedc82ab4ebe1d
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-03-13 12:59:00 +0700

    refactor findByViewId with viewBinding at ListHeroAdapter

--- app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
@@ -1,28 +1,23 @@
 package com.example.myrecyclerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myrecyclerview.databinding.ItemRowHeroBinding
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
     private lateinit var onItemClickCallback: OnItemClickCallback
 
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    class ListViewHolder(var binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
-        return ListViewHolder(view)
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
@@ -30,17 +25,12 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
         Glide.with(holder.itemView.context)
             .load(photo) // URL Gambar
             .circleCrop() // Mengubah image menjadi lingkaran
-            .into(holder.imgPhoto) // imageView mana yang akan diterapkan
-        holder.tvName.text = name
-        holder.tvDescription.text = description
-//        holder.itemView.setOnClickListener {
-//            Toast.makeText(
-//                holder.itemView.context,
-//                "Kamu memilih " + listHero[holder.adapterPosition].name,
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        }
-        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+            .into(holder.binding.imgItemPhoto) // imageView mana yang akan diterapkan
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemDescription.text = description
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
+        }
     }
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
